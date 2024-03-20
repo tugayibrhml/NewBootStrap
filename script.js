378b17d27d5b387c4de63e8d8ca5c8a77b0c88fe
tugayibrhml <tugayibrahimov@gmail.com>
2024-03-20 06:39:42 +0300

    BootStrap

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30, 
+    centeredSlides: true,
+    autoplay: {
+      delay: 2000,
+      duration: 1000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+  AOS.init();
